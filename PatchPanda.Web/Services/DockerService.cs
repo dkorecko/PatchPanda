2b462a4db67f5ad0b9389f315f45cda6eec432ff
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-10-18 14:26:49 +0200

    I guess restarting works

--- PatchPanda.Web/Services/DockerService.cs
+++ PatchPanda.Web/Services/DockerService.cs
@@ -1,4 +1,5 @@
-﻿using Docker.DotNet;
+using System.Diagnostics;
+using Docker.DotNet;
 
 namespace PatchPanda.Web.Services;
 
@@ -21,11 +22,9 @@ public class DockerService
     public async Task<IList<ContainerListResponse>> GetAllContainers()
     {
         using var dockerClient = GetClient();
-        return await dockerClient.Containers.ListContainersAsync(new ContainersListParameters
-        {
-            All = true,
-            Limit = 999
-        });
+        return await dockerClient.Containers.ListContainersAsync(
+            new ContainersListParameters { All = true, Limit = 999 }
+        );
     }
 
     public async Task<IList<ComposeStack>> GetAllComposeStacks()
@@ -34,10 +33,15 @@ public class DockerService
 
         List<ComposeStack> stacks = [];
 
-        foreach(var container in containers)
+        foreach (var container in containers)
         {
-            if (container.Labels.TryGetValue("com.docker.compose.project", out var stackName) &&
-                container.Labels.TryGetValue("com.docker.compose.config-hash", out var configHash))
+            if (
+                container.Labels.TryGetValue("com.docker.compose.project", out var stackName)
+                && container.Labels.TryGetValue(
+                    "com.docker.compose.config-hash",
+                    out var configHash
+                )
+            )
             {
                 var existingStack = stacks.FirstOrDefault(s => s.StackName == stackName);
 
@@ -46,23 +50,50 @@ public class DockerService
                     existingStack = new ComposeStack
                     {
                         StackName = stackName,
-                        ConfigFile = container.Labels.TryGetValue("com.docker.compose.project.config_files", out var configFile) ? configFile : "N/A"
+                        ConfigFile = container.Labels.TryGetValue(
+                            "com.docker.compose.project.config_files",
+                            out var configFile
+                        )
+                            ? configFile
+                            : "N/A"
                     };
 
                     stacks.Add(existingStack);
                 }
 
-                existingStack.Apps.Add(new ComposeApp
-                {
-                    Name = container.Labels.TryGetValue("com.docker.compose.service", out var appName) ? appName : container.Names.FirstOrDefault() ?? "N/A",
-                    Version = container.Labels.TryGetValue("org.opencontainers.image.version", out var appVersion) ? appVersion : "N/A",
-                    GitHubRepo = container.Labels.TryGetValue("org.opencontainers.image.source", out var appSource) ? appSource : "N/A",
-                    CurrentSha = container.ImageID,
-                    Uptime = container.Status
-                });
+                existingStack.Apps.Add(
+                    new ComposeApp
+                    {
+                        Name = container.Labels.TryGetValue(
+                            "com.docker.compose.service",
+                            out var appName
+                        )
+                            ? appName
+                            : container.Names.FirstOrDefault() ?? "N/A",
+                        Version = container.Labels.TryGetValue(
+                            "org.opencontainers.image.version",
+                            out var appVersion
+                        )
+                            ? appVersion
+                            : "N/A",
+                        GitHubRepo = container.Labels.TryGetValue(
+                            "org.opencontainers.image.source",
+                            out var appSource
+                        )
+                            ? appSource
+                            : "N/A",
+                        CurrentSha = container.ImageID,
+                        Uptime = container.Status
+                    }
+                );
             }
         }
 
         return stacks;
+    }
+
+    public async Task Restart(ComposeStack stack)
+    {
+        await Process.Start("docker", $"compose -f {stack.ConfigFile} restart").WaitForExitAsync();
     }
 }
