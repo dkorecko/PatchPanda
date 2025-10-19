8f1478448a22fa3361653b8cf1340d9b4c0baf13
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-10-19 16:42:46 +0200

    store previous version

--- PatchPanda.Web/Services/DataService.cs
+++ PatchPanda.Web/Services/DataService.cs
@@ -1,18 +1,52 @@
+using System.Text.Json;
+
 namespace PatchPanda.Web.Services;
 
-public class DataService
+public class DataService : IDisposable
 {
-    private DockerService _dockerService;
+    private readonly DockerService _dockerService;
+    private readonly ILogger<DataService> _logger;
 
-    public DataService(DockerService dockerService)
+    public DataService(DockerService dockerService, ILogger<DataService> logger)
     {
         _dockerService = dockerService;
+        _logger = logger;
+    }
+
+    public void Dispose()
+    {
+        var path = GetDataFilePath();
+        File.WriteAllText(path, JsonSerializer.Serialize(Constants.COMPOSE_APPS));
+    }
+
+    private string GetDataFilePath()
+    {
+        string path = "C:\\Users\\PC\\Coding\\self-host\\data.json";
+#if !DEBUG
+        path = "/media/data.json";
+#endif
+        return path;
     }
 
     public async Task<IEnumerable<ComposeStack>> GetData()
     {
         if (Constants.COMPOSE_APPS is null)
-            await UpdateData();
+        {
+            string path = GetDataFilePath();
+
+            if (File.Exists(path))
+            {
+                var json = await File.ReadAllTextAsync(path);
+                Constants.COMPOSE_APPS = JsonSerializer.Deserialize<IEnumerable<ComposeStack>>(
+                    json
+                );
+                _logger.LogInformation("Data retrieved from previous app run.");
+            }
+            else
+            {
+                await UpdateData();
+            }
+        }
 
         return Constants.COMPOSE_APPS!;
     }
