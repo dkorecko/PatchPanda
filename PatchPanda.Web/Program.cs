5319080489e9c0f554fa2d8f69c76be9f856c6d6
fgsfds <4870330+fgsfds@users.noreply.github.com>
2025-11-09 22:33:48 +0500

    place db in root on linux

--- PatchPanda.Web/Program.cs
+++ PatchPanda.Web/Program.cs
@@ -1,58 +1,85 @@
 using PatchPanda.Web.Components;
 using PatchPanda.Web.Services.Background;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace PatchPanda.Web;
 
-// Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-
-builder.Services.AddScoped<DockerService>();
-builder.Services.AddScoped<VersionService>();
-builder.Services.AddScoped<DiscordService>();
-builder.Services.AddScoped<UpdateService>();
-builder.Services.AddScoped<IFileService, SystemFileService>();
-builder.Services.AddSingleton<UpdateRegistry>();
-builder.Services.AddSingleton<UpdateQueue>();
-builder.Services.AddHostedService<VersionCheckHostedService>();
-builder.Services.AddHostedService<UpdateBackgroundService>();
-
-var baseUrl = builder.Configuration.GetValue<string?>("BASE_URL");
-
-Constants.BASE_URL = baseUrl is null ? null : baseUrl.TrimEnd('/');
-
-builder.Services.AddDbContextFactory<DataContext>(opt =>
+public sealed partial class Program
 {
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+        builder.Services.AddScoped<DockerService>();
+        builder.Services.AddScoped<VersionService>();
+        builder.Services.AddScoped<DiscordService>();
+        builder.Services.AddScoped<UpdateService>();
+        builder.Services.AddScoped<IFileService, SystemFileService>();
+        builder.Services.AddSingleton<UpdateRegistry>();
+        builder.Services.AddSingleton<UpdateQueue>();
+        builder.Services.AddHostedService<VersionCheckHostedService>();
+        builder.Services.AddHostedService<UpdateBackgroundService>();
+
+        var baseUrl = builder.Configuration.GetValue<string?>("BASE_URL");
+
+        Constants.BASE_URL = baseUrl is null ? null : baseUrl.TrimEnd('/');
+
 #if DEBUG
-    opt.UseSqlite("Data Source=patchpanda.db");
-    opt.EnableSensitiveDataLogging();
+        builder.Services.AddDbContextFactory<DataContext>(CreateDebugDatabase);
 #else
-    Directory.CreateDirectory("app/data");
-    opt.UseSqlite("Data Source=app/data/patchpanda.db");
+    if (OperatingSystem.IsWindows())
+    {
+        builder.Services.AddDbContextFactory<DataContext>(CreateDatabaseAtWorkingFolder);
+    }
+    else
+    {
+        builder.Services.AddDbContextFactory<DataContext>(CreateDatabaseAtRoot);
+    }
 #endif
-});
 
-var app = builder.Build();
+        var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>()!;
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>()!;
 
-    if (dbContext.Database.IsRelational())
-        dbContext.Database.Migrate();
+            if (dbContext.Database.IsRelational())
+                dbContext.Database.Migrate();
+        }
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+
+    private static void CreateDebugDatabase(DbContextOptionsBuilder opt)
+    {
+        opt.UseSqlite("Data Source=patchpanda.db");
+        opt.EnableSensitiveDataLogging();
+    }
+
+    private static void CreateDatabaseAtWorkingFolder(DbContextOptionsBuilder opt)
+    {
+        opt.UseSqlite("Data Source=patchpanda.db");
+    }
+
+    private static void CreateDatabaseAtRoot(DbContextOptionsBuilder opt)
+    {
+        Directory.CreateDirectory("/app/data");
+        opt.UseSqlite("Data Source=/app/data/patchpanda.db");
+    }
 }
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-app.MapStaticAssets();
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-
-app.Run();
