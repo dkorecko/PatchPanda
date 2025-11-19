d77ddeaeff58e362b2c50d7272568a8c93990f6e
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-11-19 18:42:04 +0100

    unused import

--- PatchPanda.Units/UpdateServiceTests.cs
+++ PatchPanda.Units/UpdateServiceTests.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.VisualBasic;
 using PatchPanda.Web.Db;
 using PatchPanda.Web.Entities;
 using PatchPanda.Web.Services;
