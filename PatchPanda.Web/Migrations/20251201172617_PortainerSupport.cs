8d12c48ebe26363d13cb84f876956172e26aba80
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-12-01 18:26:22 +0100

    migration

---
+++ PatchPanda.Web/Migrations/20251201172617_PortainerSupport.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatchPanda.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class PortainerSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ConfigFile",
+                table: "Stacks",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PortainerManaged",
+                table: "Stacks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PortainerManaged",
+                table: "Stacks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConfigFile",
+                table: "Stacks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
