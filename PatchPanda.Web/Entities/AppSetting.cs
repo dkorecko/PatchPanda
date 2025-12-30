c857b401262670ddef1aa970226ecf6729a32a38
Dávid Korečko <reset259@gmail.com>
2025-12-30 16:30:40 +0100

    small fixes

--- PatchPanda.Web/Entities/AppSetting.cs
+++ PatchPanda.Web/Entities/AppSetting.cs
@@ -1,9 +1,11 @@
 namespace PatchPanda.Web.Entities;
 
-public class AppSetting
+public class AppSetting : AbstractEntity
 {
     [Key]
+    [MaxLength(64)]
     public required string Key { get; init; }
 
+    [MaxLength(64)]
     public required string Value { get; set; }
 }
